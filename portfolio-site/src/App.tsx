f1588b95fa13c7c98eba54592b7876e78deff10a
Kyle Levisay <kyleandrewlevi43@outlook.com>
2025-11-06 20:06:49 +0900

    Created general structure of the site

---
+++ portfolio-site/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hi, I'm Kyle</h1>
+      <p>What can I build for you today?</p>
+    </>
+  );
+}
+
+export default App;
---
+++ portfolio-site/src/components/Nav.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>home</li>
+          <li>about</li>
+          <li>projects</li>
+          <li>contact</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Nav;
